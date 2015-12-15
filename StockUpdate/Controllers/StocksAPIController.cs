906b7fb3025be757b8d23897cbfc0abfaa46b550
bobmack <bobbymaceneaney@gmail.com>
2015-12-15 16:37:25 +0000

    GitUpload2

---
+++ StockUpdate/Controllers/StocksAPIController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using StockUpdate.Models;
+
+namespace StockUpdate.Controllers
+{
+    public class StocksAPIController : ApiController
+    {
+        private StockUpdateContext db = new StockUpdateContext();
+
+        // GET: api/StocksAPI
+        public IQueryable<Stock> GetStocks()
+        {
+            return db.Stocks;
+        }
+
+        // GET: api/StocksAPI/5
+        [ResponseType(typeof(Stock))]
+        public async Task<IHttpActionResult> GetStock(int id)
+        {
+            Stock stock = await db.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+
+        // PUT: api/StocksAPI/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutStock(int id, Stock stock)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != stock.StockReference)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(stock).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StockExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/StocksAPI
+        [ResponseType(typeof(Stock))]
+        public async Task<IHttpActionResult> PostStock(Stock stock)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Stocks.Add(stock);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = stock.StockReference }, stock);
+        }
+
+        // DELETE: api/StocksAPI/5
+        [ResponseType(typeof(Stock))]
+        public async Task<IHttpActionResult> DeleteStock(int id)
+        {
+            Stock stock = await db.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            db.Stocks.Remove(stock);
+            await db.SaveChangesAsync();
+
+            return Ok(stock);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StockExists(int id)
+        {
+            return db.Stocks.Count(e => e.StockReference == id) > 0;
+        }
+    }
+}
---
+++ StockUpdate/Models/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StockUpdate.Models
+{
+    public class Stock
+    {
+        [Key]
+        public int StockReference { get; set; }
+        [StringLength(20, MinimumLength = 1)]
+        public String Ticker { get; set; }
+        [StringLength(200, MinimumLength = 1)]
+        public string StockName { get; set; }
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+    }
+}
