753e94225c197fb3204d6ce3bc06c4f112cfdad0
bobmack <bobbymaceneaney@gmail.com>
2015-12-15 22:54:07 +0000

    git7

---
+++ StockUpdate.Tests/Controllers/StockControllerTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockUpdate;
+using StockUpdate.Controllers;
+using StockUpdate.Models;
+using System.Threading.Tasks;
+
+namespace StockUpdate.Tests.Controllers
+{
+    [TestClass]
+    public class StockControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            StocksController controller = new StocksController();
+
+            var result = controller.Index() as Task<ActionResult>;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            //Arrange
+            StocksController controller = new StocksController();
+            //Act
+           var result = controller.Details(2) as Task<ActionResult>;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            //Arrange
+            StocksController controller = new StocksController();
+            //Act
+           var result = controller.Create() as ViewResult;
+           // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Edit()
+        {
+            //Arrange
+            StocksController controller = new StocksController();
+            //Act
+            var result = controller.Edit(1) as Task<ActionResult>;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Delete()
+        {
+            //Arrange
+            StocksController controller = new StocksController();
+            //Act
+            var result = controller.Delete(1) as Task<ActionResult>;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ StockUpdate.Tests/UnitTestModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockUpdate.Models;
+
+namespace StockUpdate.Tests
+{
+    [TestClass]
+    public class UnitTestModel
+    {
+        [TestMethod]
+        public void TestStock()
+        {
+            Stock s = new Stock()
+            {
+                StockReference = 0,
+                Ticker = "AAPL",
+                StockName = "Apple Inc.",
+                Price = 125.59
+            };
+            Assert.AreEqual(0, s.StockReference);
+            Assert.AreEqual("AAPL", s.Ticker);
+            Assert.AreEqual("Apple Inc.", s.StockName);
+            Assert.AreEqual(125.59, s.Price);
+        }
+    }
+}
