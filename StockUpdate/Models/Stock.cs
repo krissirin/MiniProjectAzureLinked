3c0870dfac431067dc9399a87d9628d0454e48ff
krissirin <krissirin@yahoo.com>
2015-12-16 17:00:05 +0000

    Update Stock.cs
    Adding comments

--- StockUpdate/Models/Stock.cs
+++ StockUpdate/Models/Stock.cs
@@ -4,17 +4,23 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+// Stock model class
 namespace StockUpdate.Models
 {
+     // an order for stock, with data annotations
     public class Stock
     {
+        // all value types are impicility required
         [Key]
-        public int StockReference { get; set; }
-        [StringLength(20, MinimumLength = 1)]
+        public int StockReference { get; set; }     //PK
+        
+        [StringLength(20, MinimumLength = 1)]       //min 1 to max 20 chars
         public String Ticker { get; set; }
-        [StringLength(200, MinimumLength = 1)]
+        
+        [StringLength(200, MinimumLength = 1)]      //min 1 to max 200 chars
         public string StockName { get; set; }
-        [DataType(DataType.Currency)]
+        
+        [DataType(DataType.Currency)]               //display $ 
         public double Price { get; set; }
     }
 }
